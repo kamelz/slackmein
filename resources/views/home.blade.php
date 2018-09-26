92643063624665fd74a039d3060c302cd756f654
Mohamed Kamel <Mohamed.Kamel@student.aast.edu>
2018-09-27 01:47:32 +0200

    Log section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+    @if (session('status'))
+    <div class="alert alert-info" role="alert">
+        {{ session('status') }}
+    </div>
+    @endif
+
+    <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Log</div>
+            <div class="card-body">
+                @if (session('log'))
+                <pre>
+                    {{ session('log') }}
+                </pre>
+                @endif
+            </div>
+        </div>
+    </div>
+</div>
+
     <div class="row justify-content-center">
     <div class="col-md-8">
         <div class="card">
